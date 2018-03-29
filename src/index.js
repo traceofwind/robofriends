d458d7f8b4c8917e76abcfe0cbf55bf18ea1233c
traceofwind <gary@traceofwind.com>
2018-03-29 11:58:06 +0100

    robots.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import {robots} from './robots'
 
 ReactDOM.render(
 				<div>
