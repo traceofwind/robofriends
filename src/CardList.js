348034053ba3206ed0d6782fdecc2e694a352063
traceofwind <gary@traceofwind.com>
2018-03-29 12:26:25 +0100

    Created CardList.js and cardsArray

---
+++ src/CardList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+	const cardsArray = robots.map((user, i) => {
+		//.map is better than forEach
+		return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+	})
+	return (
+		<div>
+			{cardsArray}
+		</div>
+	);
+}
+
+export default CardList;
