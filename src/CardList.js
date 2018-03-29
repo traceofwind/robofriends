3809079262a4605f2b5e2418e87bac268a5de206
traceofwind <gary@traceofwind.com>
2018-03-29 12:31:41 +0100

    BONUS: key prop should have unique id: {robots[i].id}

--- src/CardList.js
+++ src/CardList.js
@@ -4,7 +4,7 @@ import Card from './Card';
 const CardList = ({ robots }) => {
 	const cardsArray = robots.map((user, i) => {
 		//.map is better than forEach
-		return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+		return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
 	})
 	return (
 		<div>
