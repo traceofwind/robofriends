9b4de80aa93bbf2892b1207722273722dd35b0b7
traceofwind <gary@traceofwind.com>
2018-03-29 11:38:06 +0100

    Added an img src

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = () => {
 	return (
 		<div>
-			<img alt='robots' src ='' />
+			<img alt='robots' src ='https://robohash.org/glhicks?size=200x200' />
 			<div>
 				<h2>Gary Hicks</h2>
 				<p>glhicks@gmail.com</p>
