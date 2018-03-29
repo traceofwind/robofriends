1c066b706bb35e359a6faa22b06abe253d5abf9b
traceofwind <gary@traceofwind.com>
2018-03-29 12:10:14 +0100

    Template Strings: img src using props.id

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = (props) => {
 	return (
-		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robots' src ='https://robohash.org/glhicks?size=200x200' />
+		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<img alt='robots' src ={`https://robohash.org/glhicks${props.id}?size=200x200`} />
 			<div>
 				<h2>{props.name}</h2>
 				<p>{props.email}</p>
