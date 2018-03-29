549ca6e598a027cdab4576e752175b9555b16832
traceofwind <gary@traceofwind.com>
2018-03-29 11:27:31 +0100

    Card.js const Card using JSX

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+	return (
+		<div>
+			<img alt='robots' src ='' />
+			<div>
+				<h2>Gary Hicks</h2>
+				<p>glhicks@gmail.com</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
