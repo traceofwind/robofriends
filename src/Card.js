454ac2bd226069a985122067ab88ae6bcf7a98d6
traceofwind <gary@traceofwind.com>
2018-03-29 12:12:23 +0100

    Destructured Card.js props into 3 x Const

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Card = (props) => {
+	//destructuring
+	const { name, email, id } = props;
 	return (
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robots' src ={`https://robohash.org/glhicks${props.id}?size=200x200`} />
+			<img alt='robots' src ={`https://robohash.org/glhicks${id}?size=200x200`} />
 			<div>
-				<h2>{props.name}</h2>
-				<p>{props.email}</p>
+				<h2>{name}</h2>
+				<p>{email}</p>
 			</div>
 		</div>
 	);
