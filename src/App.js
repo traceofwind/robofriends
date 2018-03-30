2f83c326819d5636b7802b565cdbc8db2e538648
traceofwind <gary@traceofwind.com>
2018-03-30 10:07:57 +0100

    Added comment and const state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
+const state = {
+	robots: robots,
+	searchField: ''
+}
+
 const App = () => {
 	return (
 		<div className='tc'>
 			<h1>RoboFriends</h1>
 			<SearchBox />
 			<CardList robots={ robots }/>
+			{
+			//SearchBox needs to communicate with CardList.
+			//CardList needs to know what's in SearchBox.
+			//so that it can filter.
+			//SearchBox = parent
+			//CardList = child
+
+			// STATE - an object that describes the state of our app
+			// PROPS - things that come out of state
+			// STATE >> PROPS
+			}
 		</div>
 	);
 }
