64bd4f3b2326cafb1d18464a4afc22aa5be57aaf
traceofwind <gary@traceofwind.com>
2018-03-30 10:57:19 +0100

    Comments removed,filteredRobots moved to render() + CardList

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ import { robots } from './robots';
 
 class App extends Component {
 	constructor() {
-		//in order to use this,
-		//we need to call super,
-		//which calls the constructor of component.
 		super()
-		// STATE - an object that can change and effect our app
-		// PROPS - things that come out of state
-		// STATE >> PROPS
 		this.state = {
 			robots: robots,
 			searchfield: ''
@@ -20,40 +14,19 @@ class App extends Component {
 	}
 
 	onSearchChange = (event) => {
-		//function
-		//on search change, 
-		//everytime the input changes,
-		//we get an event.
-		//within this event we will console.log this event.
-		
-		//sets the state of searchfield to its value
 		this.setState ({ searchfield: event.target.value })
+	}
 
-		//now we can communicate this value to CardsList
+	render () {
 		const filteredRobots = this.state.robots.filter(robot => {
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		//event.target.value gives us the value
-		//console.log(event.target.value);
-		console.log(filteredRobots);
-	}
-
-	//render () {} is required of class
-	render () {
 	
-		//const App = () => {
 		return (
 			<div className='tc'>
 				<h1>RoboFriends</h1>
 				<SearchBox searchChange={ this.onSearchChange }/>
-				<CardList robots={ this.state.robots }/>
-				{
-				//SearchBox needs to communicate with CardList.
-				//CardList needs to know what's in SearchBox.
-				//so that it can filter.
-				//SearchBox = parent
-				//CardList = child
-				}
+				<CardList robots={ filteredRobots }/>
 			</div>
 		);
 	}
